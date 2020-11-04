0d6319bb6c39b3afbe04db1bc445887ab99a9c76
nleanba <noam@factsmission.com>
2020-11-04 21:06:59 +0100

    base parsing

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+'use strict'
+
 // Variables
 
 const turtle = document.getElementById('turtle')
 const input = document.getElementById('input')
+const output = document.getElementById('output')
 const runButton = document.getElementById('run')
 const resetButton = document.getElementById('reset')
 const canvasCtx = document.getElementById('canvas').getContext("2d")
@@ -28,7 +31,7 @@ reset()
 
 // Functions
 
-function reset () {
+function reset() {
   state.x = 500
   state.y = 500
   state.rotation = 0
@@ -40,11 +43,50 @@ function reset () {
   canvasCtx.clearRect(0, 0, 1000, 1000)
 }
 
-function run () {
-  const code = input.value.replace(/\n/g, ' ').split(' ')
-  console.log(code)
+function fd(distance) {
+  console.log(`moving ${distance}`);
 }
 
-function move (distance) {
-  return;
+function bk (distance) {
+  fd(-distance)
 }
+
+const internalFunctions = {
+  forward: [ fd, 1 ],
+  fd: [ fd, 1 ],
+  back: [ bk, 1, ],
+  bk: [ bk, 1 ],
+}
+
+// Parser
+
+function run() {
+  const code = input.value.replace(/\n/g, ' \n ').split(' ')
+
+  let line = 1
+
+  while (code.length) {
+    const token = code.shift()
+
+    if (token === '') continue;
+    if (token === '\n') {
+      line++
+      continue
+    }
+
+    if (Object.keys(internalFunctions).includes(token)) {
+      const args = []
+      for (let i = 0; i < internalFunctions[token][1] ; i++) {
+        args.push(code.shift())
+      }
+      internalFunctions[token][0](...args)
+      continue;
+    }
+
+    const error = document.createElement('div')
+    error.classList.add('error')
+    error.textContent = `Don't know what to do with "${token}" on line ${line}`
+    output.append(error)
+  }
+}
+
